6f5f9d8b686aa33a1d4b2c8afa05d3b2bb8db94c
elduke717 <spacematcha@proton.me>
2023-07-13 15:20:00 -0400

    add some info about graphs

---
+++ graph-depth_first_traversal.js
@@ -0,0 +1,7 @@
+// Depth First Traversal in Graphs
+
+
+// Depth first traversal goes as deep as possible in a graph, then backtracks until it finds a new path to explore.  It uses a stack to keep track of the nodes to visit next.  
+
+// Breadth first traversal goes through each level of a graph, starting at the root, before moving on to the next level.  It uses a queue to keep track of the nodes to visit next.
+
